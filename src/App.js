94ffd7e487784facd05cd98bc465f7352cab3655
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-06-16 14:15:48 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React,{useState} from "react";
 import "./style.css";
 
 export default function App() {
+  const[count, setCount] = useState(0);
+
   return (
     <div>
       <h1>Use Reducer</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h2>{count}</h2>
+      <div className="">
+        <button onClick={()=>setCount(count + 1)}>Inc</button>
+        <button onClick={()=>setCount(count - 1)}>Dec</button>
+        </div>
     </div>
   );
 }
